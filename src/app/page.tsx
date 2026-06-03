9faede5dd84996dc3424ff149379d22247effa58
Jagan Kanha Dalei <daleijagan@gmail.com>
2026-06-03 22:55:41 +0530

    chore: testing the intial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { div } from 'framer-motion/client';
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="font-canopee bg-primary text-background text-shadow hack-shadow mx-auto my-auto w-2xl py-4 text-center text-5xl">
-        welcome everyone
+      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-auto w-2xl py-4 text-center text-5xl">
+        happy coding!
       </div>
     </div>
   );
