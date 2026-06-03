8a3f40564369c051e35a0f6be7ad3307d9c0fd9d
tilak-raaz <tilak4433@gmail.com>
2026-06-04 01:49:04 +0530

    fix: liniting and formating errors

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
