432cea649416d85ff5a53d0ce0017b147183d9d6
Prasad <prasadpatra234@gmail.com>
2026-06-03 23:33:58 +0530

    feat: name added in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-auto w-2xl py-4 text-center text-5xl">
+    <div className="flex h-screen flex-col items-center">
+      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto w-2xl py-4 text-center text-5xl">
         happy coding!
+      </div>
+      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-10 w-2xl py-4 text-center text-5xl">
+        Prasad
       </div>
     </div>
   );
