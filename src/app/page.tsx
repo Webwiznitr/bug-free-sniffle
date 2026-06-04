fe8a693730a45ec70a8f91f8e5bb5660dea07569
tilak-raaz <tilak4433@gmail.com>
2026-06-04 23:12:20 +0530

    fix: component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,8 @@ import Faq from '@/components/faq/Faq';
 import Sponsors from '@/components/sponsors/Sponsors';
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center">
+    <div>
       <Hero />
-      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto w-2xl py-4 text-center text-5xl">
-        happy coding!
-      </div>
-      <div className="font-canopee bg-secondary text-background text-shadow hack-shadow mx-auto my-10 w-2xl py-4 text-center text-5xl">
-        hello everyone!
-      </div>
       <Timeline />
       <Sponsors />
       <Faq />
