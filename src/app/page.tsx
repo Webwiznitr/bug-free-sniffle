17ea9ad7c148d711d43d6404c09520193a195614
Sairaj Tripathy <sairajtripathy4@gmail.com>
2026-06-04 22:50:16 +0530

    feat: added faq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from '@/components/hero/Hero';
 import Timeline from '@/components/timeline/Timeline';
+import Faq from '@/components/faq/Faq';
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center">
@@ -11,6 +12,7 @@ export default function Home() {
         hello everyone!
       </div>
       <Timeline />
+      <Faq />
     </div>
   );
 }
