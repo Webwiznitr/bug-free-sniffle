853d912814acfd5f67177ed746d206b61ad888dd
unknown <sonalirath2097@gmail.com>
2026-06-13 21:01:57 +0530

    fix: changes in timeline

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Image from 'next/image';
 import { useState } from 'react';
 import { day1Events } from './day1Data';
 import { day2Events } from './day2Data';
@@ -9,24 +9,30 @@ export default function Timeline() {
   const events = activeDay === 1 ? day1Events : day2Events;
 
   return (
-    <section className="relative min-h-[2100px] w-full overflow-x-hidden bg-[#E3442E]">
+    <section className="relative min-h-[1900px] w-full overflow-x-hidden bg-[#E3442E]">
       <div className="relative mt-[117px] h-[195px]">
         <div className="absolute top-0 left-2 h-[70px] w-[90px] sm:h-[120px] sm:w-[150px] md:left-[46px] lg:h-[195px] lg:w-[240px]">
-          <img
+          <Image
             src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780832158/icon-w_jcbk89.png"
             alt="Music Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
             className="h-full w-full object-contain"
           />
         </div>
 
-        <h1 className="font-canopee absolute top-0 left-1/2 -translate-x-1/2 text-[48px] leading-none font-normal whitespace-nowrap text-[#1A0E05] underline decoration-[#1A0E05] [text-shadow:6px_6px_0px_#F4E4B8] sm:text-[80px] md:text-[120px] lg:text-[150px] xl:text-[190px]">
+        <h1 className="font-canopee absolute top-0 left-1/2 -translate-x-1/2 text-[48px] leading-none font-normal whitespace-nowrap text-[#1A0E05] underline decoration-[#1A0E05] [text-shadow:3px_3px_0px_#F4E4B8] sm:text-[80px] md:text-[120px] lg:text-[150px] xl:text-[190px]">
           TIMELINE
         </h1>
 
         <div className="absolute top-0 right-[46px] h-[195px] w-[240.412px]">
-          <img
+          <Image
             src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780832158/icon-w_jcbk89.png"
             alt="Music Icon"
+            width={0}
+            height={0}
+            sizes="100vw"
             className="h-full w-full object-contain"
           />
         </div>
@@ -34,17 +40,23 @@ export default function Timeline() {
 
       {/* Left CD */}
       <div className="absolute top-[450px] left-[-100px] z-0 overflow-hidden sm:top-[520px] sm:left-[-50px] lg:top-[500px] lg:left-0">
-        <img
+        <Image
           src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780890788/Vector-2_esk76e.png"
           alt="Left CD"
+          width={0}
+          height={0}
+          sizes="100vw"
           className="w-[220px] scale-x-[-1] sm:w-[320px] md:w-[400px] lg:w-[480px]"
         />
       </div>
       {/* Right CD */}
       <div className="absolute top-[450px] right-[-100px] z-0 overflow-hidden sm:top-[520px] sm:right-[-50px] lg:top-[500px] lg:right-0">
-        <img
+        <Image
           src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780890788/Vector-2_esk76e.png"
           alt="Right CD"
+          width={0}
+          height={0}
+          sizes="100vw"
           className="w-[220px] sm:w-[320px] md:w-[400px] lg:w-[480px]"
         />
       </div>
@@ -52,9 +64,12 @@ export default function Timeline() {
       <div className="absolute top-[250px] left-1/2 z-10 min-h-[1180px] w-[92%] max-w-[1231px] -translate-x-1/2 bg-[#F4E4B8] pb-[80px] shadow-[12px_12px_0px_#1A0E05] md:top-[387px]">
         <div className="relative flex flex-col items-center justify-between gap-8 px-4 pt-8 md:px-[40px] md:pt-[86px] lg:flex-row">
           <div className="h-[90px] w-[110px] sm:h-[140px] sm:w-[180px] lg:h-[195px] lg:w-[240px]">
-            <img
+            <Image
               src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780832157/icon_fmdcvl.png"
               alt="music"
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-full w-full object-contain"
             />
           </div>
@@ -63,18 +78,30 @@ export default function Timeline() {
             <div className="flex overflow-hidden shadow-[4px_4px_0px_#1A0E05]">
               <button
                 onClick={() => setActiveDay(1)}
-                className="inline-flex h-[50px] cursor-pointer items-center justify-center border-[4px] border-[#1A0E05] bg-[#F4E4B8] px-[20px] transition-all duration-300 hover:scale-105 sm:h-[65px] sm:px-[30px] lg:h-[79px] lg:px-[45px]"
+                className={`inline-flex h-[50px] cursor-pointer items-center justify-center border-[4px] border-[#1A0E05] px-[20px] transition-all duration-300 sm:h-[65px] sm:px-[30px] lg:h-[79px] lg:px-[45px] ${
+                  activeDay === 1 ? 'bg-[#F4E4B8]' : 'bg-[#E3442E]'
+                }`}
               >
-                <span className="font-canopee text-[24px] text-[#E3442E] sm:text-[36px] lg:text-[48px]">
+                <span
+                  className={`font-canopee text-[24px] sm:text-[36px] lg:text-[48px] ${
+                    activeDay === 1 ? 'text-[#E3442E]' : 'text-[#F4E4B8]'
+                  }`}
+                >
                   DAY-1
                 </span>
               </button>
 
               <button
                 onClick={() => setActiveDay(2)}
-                className="inline-flex h-[50px] cursor-pointer items-center justify-center border-y-[4px] border-r-[4px] border-[#1A0E05] bg-[#E3442E] px-[20px] transition-all duration-300 hover:scale-105 sm:h-[65px] sm:px-[30px] lg:h-[79px] lg:px-[45px]"
+                className={`inline-flex h-[50px] cursor-pointer items-center justify-center border-y-[4px] border-r-[4px] border-[#1A0E05] px-[20px] transition-all duration-300 sm:h-[65px] sm:px-[30px] lg:h-[79px] lg:px-[45px] ${
+                  activeDay === 2 ? 'bg-[#F4E4B8]' : 'bg-[#E3442E]'
+                }`}
               >
-                <span className="font-canopee text-[24px] text-[#F4E4B8] sm:text-[36px] lg:text-[48px]">
+                <span
+                  className={`font-canopee text-[24px] sm:text-[36px] lg:text-[48px] ${
+                    activeDay === 2 ? 'text-[#E3442E]' : 'text-[#F4E4B8]'
+                  }`}
+                >
                   DAY-2
                 </span>
               </button>
@@ -88,9 +115,12 @@ export default function Timeline() {
           </div>
 
           <div className="h-[90px] w-[110px] sm:h-[140px] sm:w-[180px] lg:h-[195px] lg:w-[240px]">
-            <img
+            <Image
               src="https://res.cloudinary.com/du5qoczcn/image/upload/v1780832157/icon_fmdcvl.png"
               alt="music"
+              width={0}
+              height={0}
+              sizes="100vw"
               className="h-full w-full object-contain"
             />
           </div>
@@ -154,7 +184,19 @@ export default function Timeline() {
           </div>
         </div>
       </div>
-      <div className="h-[100px]"></div>
+      {/* Red region */}
+      <div className="absolute bottom-[80px] left-0 h-[100px] w-full bg-[#E3442E]"></div>
+      {/* Bottom image */}
+      <div className="absolute bottom-0 left-0 h-[130px] w-full">
+        <Image
+          src="https://res.cloudinary.com/dkie0ztb7/image/upload/q_auto/f_auto/v1781351365/bott_ke7mfb.png"
+          alt="Bottom Decoration"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-full w-full object-fill"
+        />
+      </div>
     </section>
   );
 }
