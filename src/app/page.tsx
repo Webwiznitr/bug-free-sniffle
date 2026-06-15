a3bb2297ddd38220aa591b86e9c632773476d8ef
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-06-15 21:14:10 +0530

    feat: what+statistics+music player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import Hero from '@/components/hero/Hero';
 import Timeline from '@/components/timeline/Timeline';
 import Faq from '@/components/faq/Faq';
 import Sponsors from '@/components/sponsors/Sponsors';
+import WhatAndStatistics from '@/components/whatAndStatistics/WhatAndStatistics';
+import MusicPlayer from '@/components/MusicPlayer/MusicPlayer';
+
 export default function Home() {
   return (
     <div>
       <Hero />
+      <WhatAndStatistics />
       <Timeline />
       <Sponsors />
       <Faq />
+      <MusicPlayer />
     </div>
   );
 }
