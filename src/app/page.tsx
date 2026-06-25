b98fcb3c9ec22268fae28c9fb9d58c6b6d502450
suyashaditya919 <suyashaditya919@gmail.com>
2026-06-25 19:20:53 +0530

    feat: added footer and teams components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import Faq from '@/components/faq/Faq';
 import Sponsors from '@/components/sponsors/Sponsors';
 import WhatAndStatistics from '@/components/whatAndStatistics/WhatAndStatistics';
 import MusicPlayer from '@/components/MusicPlayer/MusicPlayer';
-
 import Prizes from '@/components/prizes/Prizes';
+import Footer from '@/components/footer/Footer';
+import Teams from '@/components/teams/Teams';
 export default function Home() {
   return (
     <div className="m-0 p-0">
@@ -13,11 +14,13 @@ export default function Home() {
       <WhatAndStatistics />
       <Timeline />
       <Prizes />
+      <Teams />
       <Sponsors />
       <Faq />
       <div className="mt-89">
         <MusicPlayer />
       </div>
+      <Footer />
     </div>
   );
 }
